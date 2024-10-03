26e4ecdafc90bc9275b7e111198f27eb8d99abef
andria.gv <gvaramiaandria1@gmail.com>
2024-10-03 13:35:29 +0400

    create workspace

--- tbcHW10/hw10.playground/Contents.swift
+++ tbcHW10/hw10.playground/Contents.swift
@@ -1,1 +1,46 @@
 
+import Foundation
+
+
+// MARK: - task1
+/*   */
+
+
+// MARK: - task2
+/*  */
+
+
+// MARK: - task3
+/*  */
+
+
+// MARK: - task4
+/*  */
+
+// MARK: - task5
+/*   */
+
+// MARK: - task6
+/*   */
+
+// MARK: - task7
+/*   */
+
+// MARK: - task8
+/*   */
+
+
+// MARK: - BOUNS
+
+// MARK: - task9
+/*  */
+
+// MARK: - task10
+/*  */
+
+// MARK: - task11
+/*  */
+
+// MARK: - task12
+/*  */
+
