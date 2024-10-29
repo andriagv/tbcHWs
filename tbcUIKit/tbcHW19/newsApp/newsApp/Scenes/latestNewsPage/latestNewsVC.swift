d7cb6ab34145abd383b90e5a1a301f45cc689ce4
andria.gv <gvaramiaandria1@gmail.com>
2024-10-30 00:03:35 +0400

    add hw19 file

---
+++ tbcUIKit/tbcHW19/newsApp/newsApp/Scenes/latestNewsPage/latestNewsVC.swift
@@ -0,0 +1,29 @@
+//
+//  latestNewsVC.swift
+//  newsApp
+//
+//  Created by Apple on 29.10.24.
+//
+
+import UIKit
+
+class latestNewsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
