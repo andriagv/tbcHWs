2193a5a0f13d31079b989eee959dac12eb02362f
andria.gv <gvaramiaandria1@gmail.com>
2024-10-30 19:07:49 +0400

    create news model

---
+++ tbcUIKit/tbcHW19/newsApp/newsApp/Services/NewsService.swift
@@ -0,0 +1,8 @@
+//
+//  NewsService.swift
+//  newsApp
+//
+//  Created by Apple on 30.10.24.
+//
+
+import Foundation
---
+++ tbcUIKit/tbcHW19/newsApp/newsApp/teste.swift
@@ -0,0 +1,8 @@
+//
+//  teste.swift
+//  newsApp
+//
+//  Created by Apple on 30.10.24.
+//
+
+import Foundation
