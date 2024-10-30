1a8aa57aa9d12d38e3b4a7230e40860b63df7b72
andria.gv <gvaramiaandria1@gmail.com>
2024-10-30 19:12:58 +0400

    create News Details Page

--- tbcUIKit/tbcHW19/newsApp/newsApp/Scenes/HotUpdates/HotUpdatesViewModel.swift
+++ tbcUIKit/tbcHW19/newsApp/newsApp/Scenes/HotUpdates/HotUpdatesViewModel.swift
@@ -6,8 +6,63 @@
 //
 
 import Foundation
+import UIKit
 
+
+import Foundation
 
 final class HotUpdatesViewModel {
+    private let article: NewsArticle
     
+    init(article: NewsArticle) {
+        self.article = article
+    }
+    
+    var title: String {
+        article.title
+    }
+    
+    var author: String {
+        "Published by        \(article.author)"
+    }
+    
+    var formattedDate: String {
+        formatDate(article.publishedAt)
+    }
+    
+    var description: String {
+        article.description
+    }
+    
+    var imageUrl: URL? {
+        URL(string: article.imageUrl)
+    }
+    
+    private func formatDate(_ dateString: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        
+        if let date = dateFormatter.date(from: dateString) {
+            dateFormatter.dateFormat = "EEEE, d MMM yyyy"
+            return dateFormatter.string(from: date)
+        } else {
+            return dateString
+        }
+    }
 }
+
+extension UIColor {
+    convenience init?(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+        
+        var rgb: UInt64 = 0
+        guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else { return nil }
+        let red = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgb & 0x0000FF) / 255.0
+        self.init(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+}
+
+
