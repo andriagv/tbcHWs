edcd0c6a87f0bb4a6dd3d29e521e0a916c1f9b11
andria.gv <gvaramiaandria1@gmail.com>
2024-11-04 18:06:54 +0400

    added Question TableView Cell

--- tbcUIKit/tbcHW21/QuizApp/QuizApp/Scenes/QuizPage/Cell/QuestionTableViewCell.swift
+++ tbcUIKit/tbcHW21/QuizApp/QuizApp/Scenes/QuizPage/Cell/QuestionTableViewCell.swift
@@ -1,23 +1,48 @@
+// QuestionTableViewCell.swift
+// QuizApp
 //
-//  QuestionTableViewCell.swift
-//  QuizApp
-//
-//  Created by Apple on 03.11.24.
-//
+// Created by Apple on 03.11.24.
 
 import UIKit
 
-class QuestionTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+final class QuestionTableViewCell: UITableViewCell {
+    
+    private let containerView = UIView()
+    let label = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+        backgroundColor = .clear
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
     }
-
+    
+    private func setupUI() {
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        containerView.layer.cornerRadius = 15
+        containerView.backgroundColor = .white
+        containerView.layer.shadowRadius = 12
+        contentView.addSubview(containerView)
+        
+        label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        label.textColor = .systemPurple
+        label.textAlignment = .center
+        containerView.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 12),
+            containerView.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 16),
+            containerView.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -16),
+            containerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -12),
+            
+            label.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: containerView.centerYAnchor)
+        ])
+        selectionStyle = .none
+    }
 }
