40e0213c357cadbf69404478ad1bea2ce23d5d8d
andria.gv <gvaramiaandria1@gmail.com>
2024-11-04 18:02:10 +0400

    write Question Manager

--- tbcUIKit/tbcHW21/QuizApp/QuizApp/Services/QuestionManager/QuestionManager.swift
+++ tbcUIKit/tbcHW21/QuizApp/QuizApp/Services/QuestionManager/QuestionManager.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+final class QuestionManager {
+    func fetchQuestions(completion: @escaping ([QuestionModel]) -> Void) {
+        guard let filePath = Bundle.main.path(forResource: "Questions", ofType: "json") else {
+            print("file not found")
+            completion([])
+            return
+        }
+        
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: filePath))
+            let questionResponse = try JSONDecoder().decode(QuestionResponse.self, from: data)
+            let questions = questionResponse.results.compactMap { result -> QuestionModel? in
+                guard result.incorrectAnswers.count == 3 else {
+                    print("incorrect answers for : \(result.question)")
+                    return nil
+                }
+                var answers = result.incorrectAnswers + [result.correctAnswer]
+                answers.shuffle()
+                
+                return QuestionModel(
+                    question: result.question,
+                    A: answers[0],
+                    B: answers[1],
+                    C: answers[2],
+                    D: answers[3],
+                    answer: result.correctAnswer
+                )
+            }
+            completion(questions)
+        } catch {
+            print("Failed: \(error)")
+            completion([])
+        }
+    }
+}
