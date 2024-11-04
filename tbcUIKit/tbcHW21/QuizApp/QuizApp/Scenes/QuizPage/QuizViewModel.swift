a0f13d86928119fb8d46173fbac302820de67159
andria.gv <gvaramiaandria1@gmail.com>
2024-11-04 18:04:58 +0400

    create questions list page

--- tbcUIKit/tbcHW21/QuizApp/QuizApp/Scenes/QuizPage/QuizViewModel.swift
+++ tbcUIKit/tbcHW21/QuizApp/QuizApp/Scenes/QuizPage/QuizViewModel.swift
@@ -1,8 +1,33 @@
+// QuizViewModel.swift
+// QuizApp
 //
-//  QuizViewModel.swift
-//  QuizApp
-//
-//  Created by Apple on 03.11.24.
-//
+// Created by Apple on 03.11.24.
 
 import Foundation
+
+final class QuizViewModel {
+    private var questions: [QuestionModel] = []
+    
+    func loadQuestions(completion: @escaping () -> Void) {
+        let questionManager = QuestionManager()
+        questionManager.fetchQuestions { [weak self] fetchedQuestions in
+            self?.questions = fetchedQuestions
+            completion()
+        }
+    }
+    
+    func numberOfQuestions() -> Int {
+        return questions.count
+    }
+    
+    func question(at index: Int) -> QuestionModel {
+        return questions[index]
+    }
+    
+    func resetAllAnswers() {
+        for question in questions {
+            let userDefaultsKey = "userAnswer_\(question.question)"
+            UserDefaults.standard.removeObject(forKey: userDefaultsKey)
+        }
+    }
+}
