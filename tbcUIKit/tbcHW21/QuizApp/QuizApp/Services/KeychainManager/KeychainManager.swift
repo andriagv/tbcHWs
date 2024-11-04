7dd72c3502202c7a6faca8a259632d62e144facd
andria.gv <gvaramiaandria1@gmail.com>
2024-11-04 18:02:44 +0400

    write KeychainManager

--- tbcUIKit/tbcHW21/QuizApp/QuizApp/Services/KeychainManager/KeychainManager.swift
+++ tbcUIKit/tbcHW21/QuizApp/QuizApp/Services/KeychainManager/KeychainManager.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+
+
+final class KeychainManager {
+    
+    enum KeyChainError: Error {
+        case sameItemFound
+        case unknown
+        case nosuchDataFound
+        case KCErrorWithCode(Int)
+    }
+    
+    func save(
+        service: String,
+        account: String,
+        password: Data
+    ) throws {
+        let query: [String: AnyObject] = [
+            kSecClass as String         : kSecClassGenericPassword,
+            kSecAttrService as String   : service as AnyObject,
+            kSecAttrAccount as String   : account as AnyObject,
+            kSecValueData as String     : password as AnyObject,
+        ]
+        
+        let status = SecItemAdd(query as CFDictionary, nil)
+        
+        guard status != errSecDuplicateItem else {
+            throw KeyChainError.sameItemFound
+        }
+        
+        guard status == errSecSuccess else {
+            throw KeyChainError.unknown
+        }
+    }
+    
+    func get(service: String,account: String) -> Data? {
+        let query: [String: AnyObject] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service as AnyObject,
+            kSecAttrAccount as String: account as AnyObject,
+            kSecReturnData as String: kCFBooleanTrue as AnyObject,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+        
+        var result: AnyObject?
+        
+        let status = SecItemCopyMatching(query as CFDictionary, &result)
+        
+        print("read status \(status)")
+        return result as? Data
+    }
+}
