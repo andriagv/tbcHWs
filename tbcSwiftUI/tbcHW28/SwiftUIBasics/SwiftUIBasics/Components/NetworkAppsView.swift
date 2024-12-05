4c14c9fecfcfb8de93582c3cdfec545f725845c2
andria.gv <gvaramiaandria1@gmail.com>
2024-12-06 03:46:42 +0400

    add: network app view

---
+++ tbcSwiftUI/tbcHW28/SwiftUIBasics/SwiftUIBasics/Components/NetworkAppsView.swift
@@ -0,0 +1,29 @@
+//
+//  NetworkAppsView.swift
+//  SwiftUIBasics
+//
+//  Created by Apple on 06.12.24.
+//
+
+import SwiftUI
+
+struct NetworkAppsView: View {
+    var body: some View {
+        HStack(alignment: .top, spacing: 30) {
+            ForEach(["phone", "envelope", "globe"], id: \.self) { iconName in
+                ZStack {
+                    Circle()
+                        .frame(height: 40)
+                        .foregroundColor(Color.black.opacity(0.2))
+                    Image(systemName: iconName)
+                        .frame(height: 20)
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    NetworkAppsView()
+}
