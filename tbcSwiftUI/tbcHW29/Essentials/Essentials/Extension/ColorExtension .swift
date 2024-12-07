4a1fd8a692a430b70c4bdaa9638bef4ee2bca8c7
andria.gv <gvaramiaandria1@gmail.com>
2024-12-08 01:46:50 +0400

    write color extension

---
+++ tbcSwiftUI/tbcHW29/Essentials/Essentials/Extension/ColorExtension .swift
@@ -0,0 +1,22 @@
+//
+//  ColorExtension .swift
+//  SwiftUIBasics
+//
+//  Created by Apple on 06.12.24.
+//
+
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        _ = scanner.scanString("#")
+        var rgb: UInt64 = 0
+        scanner.scanHexInt64(&rgb)
+        let r = Double((rgb >> 16) & 0xFF) / 255.0
+        let g = Double((rgb >> 8) & 0xFF) / 255.0
+        let b = Double(rgb & 0xFF) / 255.0
+        let a = hex.count > 7 ? Double((rgb >> 24) & 0xFF) / 255.0 : 1.0
+        self.init(.sRGB, red: r, green: g, blue: b, opacity: a)
+    }
+}
