d509b01311f1d9c0a41eaaecbb979e5ccbb6eec3
andria.gv <gvaramiaandria1@gmail.com>
2024-12-10 23:37:14 +0400

    initial commit

---
+++ tbcSwiftUI/tbcHW30/DataFlow/DataFlow/DataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataFlowApp.swift
+//  DataFlow
+//
+//  Created by Apple on 10.12.24.
+//
+
+import SwiftUI
+
+@main
+struct DataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
