0d8d963131bb01eac671a30af39f510244c3057a
andria.gv <gvaramiaandria1@gmail.com>
2024-12-11 18:29:48 +0400

    complite ContentView

--- tbcSwiftUI/tbcHW30/DataFlow/DataFlow/ContentView.swift
+++ tbcSwiftUI/tbcHW30/DataFlow/DataFlow/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var viewModel = TimerViewModel()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            HStack {
+                Text("ტაიმერები")
+                    .font(.largeTitle)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .makeTextStyle(color: .white, size: 24, font: nil)
+                Spacer()
+            }
+            .padding()
+            .background(Color.cardBackgroundColor)
+
+            ScrollView {
+                VStack (spacing: 20) {
+                    ForEach(viewModel.timers) { timer in
+                        TimerCardView(timer: timer, viewModel: viewModel)
+                            .padding(.horizontal)
+                    }
+                }
+            }
+        
+            AddTimerView(viewModel: viewModel)
+            
         }
-        .padding()
+        .background(.black)
     }
 }
 
