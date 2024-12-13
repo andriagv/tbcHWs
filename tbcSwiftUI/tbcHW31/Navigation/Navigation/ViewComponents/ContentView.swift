db28eb3b6caa1d1a1441aa8135e578294f5bd6af
andria.gv <gvaramiaandria1@gmail.com>
2024-12-13 16:22:18 +0400

    add navigation

--- tbcSwiftUI/tbcHW31/Navigation/Navigation/ViewComponents/ContentView.swift
+++ tbcSwiftUI/tbcHW31/Navigation/Navigation/ViewComponents/ContentView.swift
@@ -9,29 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject private var viewModel = TimerViewModel()
-
+    
     var body: some View {
-        VStack(spacing: 20) {
-            HStack {
-                Text("ტაიმერები")
-                    .font(.largeTitle)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .makeTextStyle(color: .white, size: 24, font: nil)
-                Spacer()
-            }
-            .padding()
-            .background(Color.cardBackgroundColor)
-            ScrollView {
-                VStack (spacing: 20) {
-                    ForEach(viewModel.timers) { timer in
-                        TimerCardView(timer: timer, viewModel: viewModel)
+        NavigationView {
+            VStack(spacing: 20) {
+                HStack {
+                    Text("ტაიმერები")
+                        .font(.largeTitle)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .makeTextStyle(color: .white, size: 24, font: nil)
+                    Spacer()
+                }
+                .padding()
+                .background(Color.cardBackgroundColor)
+                ScrollView {
+                    VStack (spacing: 20) {
+                        ForEach(viewModel.timers) { timer in
+                            NavigationLink {
+                                ActivityDetailsView(timer: timer)
+                            } label: {
+                                TimerCardView(timer: timer, viewModel: viewModel)
+                            }
                             .padding(.horizontal)
+                        }
                     }
                 }
+                AddTimerView(viewModel: viewModel)
             }
-            AddTimerView(viewModel: viewModel)
+            .background(.black)
         }
-        .background(.black)
     }
 }
 
