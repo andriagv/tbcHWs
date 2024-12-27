07d42d17a883fca85103b7daf8c0f50e929ff4bd
andria.gv <gvaramiaandria1@gmail.com>
2024-12-27 14:01:40 +0400

    write question model

---
+++ tbcSwiftUI/tbcHW35/EmojiRiddle/EmojiRiddle/Model/QuestionModel.swift
@@ -0,0 +1,24 @@
+//
+//  QuestionModel.swift
+//  EmojiRiddle
+//
+//  Created by Apple on 27.12.24.
+//
+
+import Foundation
+
+struct QuestionModel: Codable, Hashable {
+    let type: Type
+    let emoji: String
+    let answers: [String]
+    let correctAnswer: String
+}
+
+enum Type: String, Codable {
+    case film = "film"
+    case book = "book"
+    case anime = "anime"
+}
+
+
+
